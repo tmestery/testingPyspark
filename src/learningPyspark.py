ebe7bb94bad8e7dbc2f95566e565aedaf3db03f0
Tyler Mestery <tyler@mestery.com>
2025-12-08 23:39:53 -0600

    Added sout to learn throughout.

--- src/learningPyspark.py
+++ src/learningPyspark.py
@@ -7,43 +7,47 @@ spark = SparkSession.builder.getOrCreate()
 df = spark.read.csv('data/sample_data.csv')
 
 # Show a preview
+print("Show a preview: ")
 df.show()
 
 # Show preview of first / last n rows
+print("Show preview of first / last n rows: ")
 df.head(5)
 df.tail(5)
 
 # Show preview as JSON (WARNING: in-memory)
+print("how preview as JSON (WARNING: in-memory): ")
 df = df.limit(10) # optional
 print(json.dumps([row.asDict(recursive=True) for row in df.collect()], indent=2))
 
 # Limit actual DataFrame to n rows (non-deterministic)
+print("Limit actual DataFrame to n rows (non-deterministic)")
 df = df.limit(5)
 
 # Get columns
+print("Get columns: ")
 df.columns
 
 # Get columns + column types
+print("Get columns + column types: ")
 df.dtypes
 
 # Get schema
+print("Get schema: ")
 df.schema
 
 # Get row count
+print("Get row count: ")
 df.count()
 
 # Get column count
+print("Get column count: ")
 len(df.columns)
 
 # Write output to disk
+print("Write output to disk: ")
 df.write.csv('data/output')
 
 # Get results (WARNING: in-memory) as list of PySpark Rows
+print("Get results (WARNING: in-memory) as list of PySpark Rows: ")
 df = df.collect()
-
-# # Get results (WARNING: in-memory) as list of Python dicts
-# rows = df.collect()
-# dicts = [row.asDict(recursive=True) for row in rows]
-
-# # Convert (WARNING: in-memory) to Pandas DataFrame
-# df = df.toPandas()
